91e4bf420e589decc05caddf22a1cf04bce9b87d
Tania Liu <125942040+tania-liu@users.noreply.github.com>
2026-09-17 11:46:03 +0800

    init project

---
+++ DailyFoodRecord/DailyFoodRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyFoodRecord
+//
+//  Created by BSMAC00017 on 2026/9/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
